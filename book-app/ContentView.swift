6677e9fbcd51da520cac600d87e8f9298fc97fa2
hangoofy <tomi@joinhangoo.com>
2022-07-15 09:51:11 -0400

    Initial Commit

---
+++ book-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  book-app
+//
+//  Created by Tomi Antoljak on 7/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ book-app/book_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  book_appApp.swift
+//  book-app
+//
+//  Created by Tomi Antoljak on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct book_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
